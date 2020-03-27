3030fdbec701613c8a56bffc2dcf9bd68ecd8069
Jan Sadlek <nightkeyman@gmail.com>
2020-03-27 13:53:35 +0100

    Added setup environment script

---
+++ drums.ps1
@@ -0,0 +1,3 @@
+& 'C:\Program Files (x86)\Tobias Erichsen\loopMIDI\loopMIDI.exe'
+& 'D:\Jasiek\Workspace\EDrums\hairless-midiserial-0.4-windows\hairless-midiserial\hairless-midiserial.exe'
+& 'C:\Program Files\REAPER (x64)\reaper.exe' D:\Nightkeyman\Perkusja\Drums_MIDI.rpp
