1307be04123e19ec516093589dbef150de4a5a37
nightkeyman <nightkeyman@gmail.com>
2022-03-15 18:41:37 +0100

    Update data visualization script

--- TestData/dataVisualizer.py
+++ TestData/dataVisualizer.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+
+##### Test Values ######
 values = []
 
-f = open('3hits_ManyTimes_CymbalCrash_NoDelay.txt','r')
+cwd = os.path.dirname(os.path.realpath(__file__))
+test_file_name = '3hits_ManyTimes_CymbalCrash_NoDelay.txt'
+
+f = open(os.path.join(cwd, test_file_name),'r')
 for row in f:
     try:
         rowAsNumber = float(row)
@@ -11,34 +17,46 @@ for row in f:
         print("Value error occured")
     values.append(rowAsNumber)
 
-# Y axis parameter:
-yaxis = np.array(values)
-
 ##### Filtering ######
 param = 0.1
-filteredValues = [None] * (len(values) + 1)
+idle = 338.0
+filteredValues = [0.0] * (len(values) + 1)
+absoluteValues = [0.0] * (len(values) + 1)
 
 for i, value in enumerate(values, start=1):
     try:
         # filter equation
         filteredValues[i] = ((1 - param) * filteredValues[i-1]) + param * value
+        absoluteValues[i] = abs(filteredValues[i] - idle)
     except:
         print(i)
-        filteredValues[i] = value
+        filteredValues[i] = abs(value)
+
+# Y axis parameter:
+yaxis = np.array(values)
+# plotting - unfiltered
+plt.subplot(1, 3, 1)
+plt.title("Unfiltered values")
+plt.plot(yaxis)
+plt.ylim([250, 450])
+plt.xlim([4100, 4500])
 
 # Y axis parameter:
 yaxisFiltered = np.array(filteredValues)
-
-
-# plotting - unfiltered
-plt.subplot(1, 2, 1)
-plt.title("Unfiltered values")
-plt.plot(yaxis)
-plt.ylim([0, 800])
-
 # plotting - filtered
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.title("Values filtered by IIR")
 plt.plot(yaxisFiltered)
-plt.ylim([0, 800])
+plt.ylim([250, 450])
+plt.xlim([4100, 4500])
+
+# Y axis parameter:
+yaxisAbsolute = np.array(absoluteValues)
+# plotting - absolute filtered
+plt.subplot(1, 3, 3)
+plt.title("Absolute filtered values")
+plt.plot(yaxisAbsolute)
+plt.ylim([0, 40])
+plt.xlim([4100, 4500])
+
 plt.show()
